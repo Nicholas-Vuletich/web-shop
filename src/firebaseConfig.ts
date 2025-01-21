908e30c2a27698ce8c621c8b0b37e0d8b81166c3
Nikola Vuletić <nikolavuletic412@gmail.com>
2025-01-21 04:56:01 +0100

    sakriveni apy key

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDu2BHGFdN8qDyfMO6evVkhtEeF9hFYkWA",
-  authDomain: "web-shop-91204.firebaseapp.com",
-  projectId: "web-shop-91204",
-  storageBucket: "web-shop-91204.firebasestorage.app",
-  messagingSenderId: "1057479873509",
-  appId: "1:1057479873509:web:87f0a71b3cb92dea64950c",
-  measurementId: "G-MZ1RZLT1PV"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
