0991eda1ed46b2d034c5498dd4ac4d0097abdd99
Nikola Vuletić <nikolavuletic412@gmail.com>
2025-01-31 02:33:15 +0100

    dodana ikona kosarice sa privremenom funkcijom brojaca artikala

--- src/components/pristupnaTraka/ActionBar.tsx
+++ src/components/pristupnaTraka/ActionBar.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './ActionBar.css';
+import { FaShoppingCart } from "react-icons/fa";
 
 const ActionBar: React.FC = () => {
   const navigate = useNavigate();
+  const [cartCount, setCartCount] = useState(0); // useState mora biti unutar glavne komponente
+
+  const addToCart = () => {
+    setCartCount(cartCount + 1);
+  };
+
   return (
     <div className="action-bar">
       <div className="buttons-container">
         <button className="action-button" onClick={() => navigate('/registration')}>Registracija</button>
         <button className="action-button">Prijava</button>
+        <div className="cart-icon">
+          <FaShoppingCart size={24} />
+          {cartCount > 0 && <span className="cart-count">{cartCount}</span>}
+        </div>
+        <button className="add-to-cart-button" onClick={addToCart}>
+        Dodaj u košaricu
+</button>
+
       </div>
     </div>
   );
 };
 
 export default ActionBar;
-
